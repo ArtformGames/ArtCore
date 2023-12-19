f666f772fe1e0d4deae28c218692f89cd9571e55
carm <carm@carm.cc>
2023-12-19 23:12:38 +0800

    feat(loader): Add temp loader for temp handlers.

--- api/src/main/java/com/artformgames/core/user/manager/UserHandlerManager.java
+++ api/src/main/java/com/artformgames/core/user/manager/UserHandlerManager.java
@@ -1,15 +1,25 @@
 package com.artformgames.core.user.manager;
 
+import com.artformgames.core.user.User;
 import com.artformgames.core.user.handler.AbstractUserHandler;
+import com.artformgames.core.user.handler.TempHandlerLoader;
 import com.artformgames.core.user.handler.UserHandler;
 import com.artformgames.core.user.handler.UserHandlerLoader;
+import org.bukkit.plugin.Plugin;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.function.Function;
 
 public interface UserHandlerManager {
 
     boolean containsHandler(@NotNull Class<? extends UserHandler> handlerClazz);
 
     <H extends AbstractUserHandler> void registerHandler(@NotNull UserHandlerLoader<H> loader);
+
+    default <H extends AbstractUserHandler> void registerHandler(@NotNull Plugin plugin, @NotNull Class<H> handlerClass,
+                                                                 @NotNull Function<User, H> loader) {
+        registerHandler(TempHandlerLoader.of(plugin, handlerClass, loader));
+    }
 
     void unregisterHandler(@NotNull Class<? extends UserHandler> handlerClazz);
 
