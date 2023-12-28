545a406111d55e67c7ac903bc615470cae74168d
carm <carm@carm.cc>
2023-12-28 17:28:52 +0800

    build(deps): Update dependencies

--- api/src/main/java/com/artformgames/core/ArtCore.java
+++ api/src/main/java/com/artformgames/core/ArtCore.java
@@ -3,6 +3,8 @@ package com.artformgames.core;
 import cc.carm.lib.easysql.api.SQLManager;
 import com.artformgames.core.command.handlers.CommandUsageHandler;
 import com.artformgames.core.command.handlers.PermMissedHandler;
+import com.artformgames.core.user.User;
+import com.artformgames.core.user.handler.UserHandler;
 import com.artformgames.core.user.manager.UserManager;
 import dev.rollczi.litecommands.LiteCommandsBuilder;
 import dev.rollczi.litecommands.bukkit.LiteBukkitSettings;
@@ -10,7 +12,9 @@ import dev.rollczi.litecommands.bukkit.LiteCommandsBukkit;
 import io.github.leonardosnt.bungeechannelapi.BungeeChannelApi;
 import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
+import org.jetbrains.annotations.NotNull;
 
 public class ArtCore {
 
@@ -41,6 +45,15 @@ public class ArtCore {
 
     public static UserManager<?> getUserManager() {
         return plugin().getUserManager();
+    }
+
+    public static User getUser(@NotNull Player player) {
+        return getUserManager().get(player);
+    }
+
+    public static <T extends UserHandler> @NotNull T getHandler(@NotNull Player player,
+                                                                @NotNull Class<T> handler) {
+        return getUser(player).getHandler(handler);
     }
 
     public static BungeeChannelApi getBungeeAPI() {
